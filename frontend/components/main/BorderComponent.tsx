aa5438c94973508b8cf3ad774c80313d33e9af00
Daniel Parra <daniel27parra@gmail.com>
2024-02-29 12:26:46 -0500

    Created color queue to make overlapping borders

--- frontend/components/main/BorderComponent.tsx
+++ frontend/components/main/BorderComponent.tsx
@@ -4,21 +4,79 @@ import { View, StyleSheet, useWindowDimensions } from 'react-native';
 
 interface BorderComponentProps {
   id: string;
-  borderColor: string;
+  colorQueue: Array<[string, number, string, boolean]>;
   orientation: 'horizontal' | 'vertical'; // Specify the valid values for orientation
   lastRow: boolean;
 }
 
-const BorderComponent: React.FC<BorderComponentProps> = ({ borderColor, orientation, lastRow }) => {
+const BorderComponent: React.FC<BorderComponentProps> = ({ colorQueue, orientation, lastRow }) => {
   const borderStyle = orientation === 'horizontal' ? styles.horizontalBorder : styles.verticalBorder;
 
   const windowWidth = useWindowDimensions().width;
   const windowHeight = useWindowDimensions().height;
 
-  return <View style={[{ borderColor }, borderStyle, { width: orientation === 'horizontal' ? (windowWidth / 7) * 0.9 : 0, height: orientation === 'vertical' ? (windowHeight / 6) * 0.8 : lastRow ? (windowHeight / 6) * 0.8 + 50 : 0, paddingTop: orientation === 'horizontal' ? (windowHeight / 6) * 0.8 -1.5 : lastRow ? (windowHeight / 6) * 0.8 + 50.5 : 0 }]} />;
+  const renderBorders = () => {
+    return colorQueue.map((value, index) => {
+      const borderColor = value[0], amountFill = value[1], rootTaskId = value[2], leftBound = value[3]
+      const borderStyle = orientation === 'horizontal' ? styles.horizontalBorder : styles.verticalBorder;
+      const containerStyle = orientation === 'horizontal' ? (leftBound? styles.HLContainer : styles.HRContainer) : styles.VContainer
+      const zIndex = -(index + 1); // Set zIndex to stack borders
+
+      return (
+        <View style={[containerStyle, {
+          width: orientation === 'horizontal' ? ((windowWidth / 7) * 0.85) : 0,
+          height: orientation === 'vertical' ? ((windowHeight / 6) * 0.8) : lastRow ? ((windowHeight / 6) * 0.8 + 50) : 0
+        }]}>
+          <View
+            key={index}
+            style={[
+              { borderColor, zIndex },
+              borderStyle,
+              {
+                minHeight: 0,
+                width: orientation === 'horizontal' ? ((windowWidth / 7) * 0.85)*amountFill : 0,
+                height: orientation === 'vertical' ? ((windowHeight / 6) * 0.8)*amountFill : lastRow ? ((windowHeight / 6) * 0.8 + 50)*amountFill : 0,
+                paddingTop: orientation === 'horizontal' ? (windowHeight / 6) * 0.8 - 1.5 : lastRow ? (windowHeight / 6) * 0.8 + 50.5 : 0,
+              },
+            ]}
+          />
+        </View>
+      );
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderBorders()}
+      <View style={[ borderStyle, {zIndex: -999, borderColor: 'rgba(255, 255, 255, 0)', width: orientation === 'horizontal' ? (windowWidth / 7) * 0.9 : 0, height: orientation === 'vertical' ? (windowHeight / 6) * 0.8 : lastRow ? (windowHeight / 6) * 0.8 + 50 : 0, paddingTop: orientation === 'horizontal' ? (windowHeight / 6) * 0.8 -1.5 : lastRow ? (windowHeight / 6) * 0.8 + 50.5 : 0 }]} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  HLContainer: {
+    flex:1,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  HRContainer: {
+    flex:1,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  VContainer: {
+    position: 'absolute',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   border: {
     flex: 1,
   },
--- frontend/components/main/GridComponent.tsx
+++ frontend/components/main/GridComponent.tsx
@@ -19,12 +19,12 @@ const GridComponent: React.FC<GridProps> = ({ offset, subtaskDispIds }) => {
   }, [subtaskDispIds]);
 
 
-  const generatePathsForIds = (ids : string[]): { [key: string]: Set<[string, number, string]> } => // Returns array of #COLORHEX, Amount fill (0-1), root_task_id
+  const generatePathsForIds = (ids : string[]): { [key: string]: Set<[string, number, string, boolean]> } => // Returns array of #COLORHEX, Amount fill (0-1), root_task_id
   {
     /*
       This function assumes the parent id index has already been calculated and the subtask indices have also been calculated
     */
-    var colorQueuesMap:{[key: string]: Set<[string, number, string]>} = {}
+    var colorQueuesMap:{[key: string]: Set<[string, number, string, boolean]>} = {}
 
     for(const id of ids)
     {
@@ -65,14 +65,46 @@ const GridComponent: React.FC<GridProps> = ({ offset, subtaskDispIds }) => {
         // Create key values for horizontal borders to fill in row of subtask and fill queus 
         if(prioritized)
         {
+          // Draw line from calendar node to 
           if(colorQueuesMap.hasOwnProperty(`${row}h${column}`))
           {
-            colorQueuesMap[`${row}h${column}`].add([hexcode, 0.25 * index, rootId])
+            colorQueuesMap[`${row}h${column}`].add([hexcode, 0.25 * index, rootId, leftBound])
           }
           else
           {
-            colorQueuesMap[`${row}h${column}`] = new Set([[hexcode, 0.25 * index, rootId]])
-          } 
+            colorQueuesMap[`${row}h${column}`] = new Set([[hexcode, 0.25 * index, rootId, leftBound]])
+          }
+         
+          // Draw horizontal lines for second level subtasks 
+          var columnOffset:number = column
+          while(leftBound ? columnOffset > 0 : columnOffset < 6)
+          {
+            leftBound ? columnOffset -= 1 : columnOffset += 1;
+            if(colorQueuesMap.hasOwnProperty(`${row}h${columnOffset}`))
+            {
+              colorQueuesMap[`${row}h${columnOffset}`].add([hexcode, 1, rootId, leftBound])
+            }
+            else
+            {
+              colorQueuesMap[`${row}h${columnOffset}`] = new Set([[hexcode, 1, rootId, leftBound]])
+            }
+          }
+
+          // Draw vertical lines
+          var rowOffset:number = row
+          while(rowOffset < 4)
+          {
+            rowOffset += 1
+            if(colorQueuesMap.hasOwnProperty(`${rowOffset}h${columnOffset}`))
+            {
+              colorQueuesMap[`${rowOffset}v${columnOffset}`].add([hexcode, 1, rootId, leftBound])
+            }
+            else
+            {
+              colorQueuesMap[`${rowOffset}v${columnOffset}`] = new Set([[hexcode, 1, rootId, leftBound]])
+            }
+          }
+
         }
         else
         {
@@ -96,124 +128,124 @@ const GridComponent: React.FC<GridProps> = ({ offset, subtaskDispIds }) => {
   return (
     <View style={[styles.grid, {marginTop: offset * 8}]}>
       <View style={styles.row}>
-        <BorderComponent id='0v0' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h0' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v0' colorQueue={[['red',1,"lol", false],['green',0.5,"lol", false]]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h0' colorQueue={[['red',1,"lol", true], ['green',0.5,"lol", true]]} orientation="horizontal" lastRow = {false}  />
 
-        <BorderComponent id='0v1' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h1' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v1' colorQueue={[]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h1' colorQueue={[]} orientation="horizontal" lastRow = {false}  />
  
-        <BorderComponent id='0v2' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h2' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v2' colorQueue={[['red',1,"lol", false]]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h2' colorQueue={[]} orientation="horizontal" lastRow = {false}  />
 
-        <BorderComponent id='0v3' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h3' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v3' colorQueue={[['red',1,"lol", false]]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h3' colorQueue={[]} orientation="horizontal" lastRow = {false}  />
 
-        <BorderComponent id='0v4' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h4' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v4' colorQueue={[]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h4' colorQueue={[['red',1,"lol", false]]} orientation="horizontal" lastRow = {false}  />
 
-        <BorderComponent id='0v5' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h5' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v5' colorQueue={[]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h5' colorQueue={[]} orientation="horizontal" lastRow = {false}  />
 
-        <BorderComponent id='0v6' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
-        <BorderComponent id='0h6' borderColor="#BA6767" orientation="horizontal" lastRow = {false}  />
+        <BorderComponent id='0v6' colorQueue={[]} orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0h6' colorQueue={[]} orientation="horizontal" lastRow = {false}  />
 
-        <BorderComponent id='0v7' borderColor="#78BA67" orientation="vertical"  lastRow = {false} />
+        <BorderComponent id='0v7' colorQueue={[]} orientation="vertical"  lastRow = {false} />
       </View>
       <View style={styles.row}>
-        <BorderComponent id='1v0' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h0' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v0' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h0' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v1' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h1' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v1' colorQueue={[['red',1,"lol", false]]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h1' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v2' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h2' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v2' colorQueue={[['red',1,"lol", false]]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h2' colorQueue={[['red',1,"lol", false]]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v3' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h3' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v3' colorQueue={[['red',1,"lol", false]]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h3' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v4' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h4' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v4' colorQueue={[['red',1,"lol", false]]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h4' colorQueue={[['red',1,"lol", false]]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v5' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h5' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v5' colorQueue={[['red',1,"lol", false]]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h5' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v6' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='1h6' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='1v6' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1h6' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='1v7' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
+        <BorderComponent id='1v7' colorQueue={[]} orientation="vertical" lastRow = {false} />
       </View>
       <View style={styles.row}>
-        <BorderComponent id='2v0' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h0' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v0' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h0' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v1' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h1' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v1' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h1' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v2' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h2' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v2' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h2' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v3' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h3' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v3' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h3' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v4' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h4' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v4' colorQueue={[['red',1,"lol", false]]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h4' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v5' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h5' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v5' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h5' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v6' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='2h6' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='2v6' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2h6' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='2v7' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
+        <BorderComponent id='2v7' colorQueue={[]} orientation="vertical" lastRow = {false} />
       </View>
       <View style={styles.row}>
-        <BorderComponent id='3v0' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h0' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v0' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h0' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v1' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h1' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v1' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h1' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v2' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h2' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v2' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h2' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v3' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h3' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v3' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h3' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v4' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h4' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v4' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h4' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v5' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h5' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v5' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h5' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v6' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
-        <BorderComponent id='3h6' borderColor="#BA6767" orientation="horizontal" lastRow = {false} />
+        <BorderComponent id='3v6' colorQueue={[]} orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3h6' colorQueue={[]} orientation="horizontal" lastRow = {false} />
 
-        <BorderComponent id='3v7' borderColor="#78BA67" orientation="vertical" lastRow = {false} />
+        <BorderComponent id='3v7' colorQueue={[]} orientation="vertical" lastRow = {false} />
       </View>
       <View style={styles.row}>
-        <BorderComponent id='4v0' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h0' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v0' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h0' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v1' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h1' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v1' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h1' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v2' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h2' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v2' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h2' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v3' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h3' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v3' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h3' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v4' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h4' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v4' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h4' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v5' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h5' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v5' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h5' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v6' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
-        <BorderComponent id='4h6' borderColor="#BA6767" orientation="horizontal" lastRow = {true} />
+        <BorderComponent id='4v6' colorQueue={[]} orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4h6' colorQueue={[]} orientation="horizontal" lastRow = {true} />
 
-        <BorderComponent id='4v7' borderColor="#78BA67" orientation="vertical" lastRow = {true} />
+        <BorderComponent id='4v7' colorQueue={[]} orientation="vertical" lastRow = {true} />
       </View>
     </View>
   );
@@ -226,6 +258,7 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
+    alignItems: 'flex-end'
   },
 });
 
