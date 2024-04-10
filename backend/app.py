e050989242fab18a94840e21e67dfaffc82e8eff
Rohan Shah <rshah04@gmail.com>
2024-04-10 11:19:56 -0400

    added getTask backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
+import firebase_admin
 from flask import Flask, request, jsonify
 from firebase_auth import auth
 from flask_cors import CORS
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('lifehappens-293da-firebase-adminsdk-77os9-bdebc62647.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -37,7 +45,7 @@ def login():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-# Dummy data route
+# Dummy data route  
 @app.route('/data')
 def get_time():
     # Returning dummy data
@@ -48,6 +56,29 @@ def get_time():
         "programming": "python"
     }
 
+
+
+def get_task_by_user_and_task_id(user_id, task_id):
+    try:
+        # Navigating to the Task document within the User subcollection
+        task_ref = db.collection('User').document(user_id).collection('Tasks').document(task_id)
+        task = task_ref.get()
+        if task.exists:
+            return task.to_dict()
+        else:
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+# Route to get a specific task for a user
+@app.route('/user/<user_id>/task/<task_id>', methods=['GET'])
+def get_user_task(user_id, task_id):
+    task = get_task_by_user_and_task_id(user_id, task_id)
+    if task:
+        return jsonify(task), 200
+    else:
+        return jsonify({'error': 'Task not found'}), 404
 # Run Flask app
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
