c9bfff225d3fc648e89afc5f772708d4bc566d88
Rohan Shah <rshah04@gmail.com>
2024-04-22 16:50:54 -0400

    added Delete Task

--- backend/ai_funcs.py
+++ backend/ai_funcs.py
@@ -30,10 +30,10 @@ from file_extract_tools import extract_text_from_pdf_byte_content, extract_text_
 load_dotenv()
 OPENAI_APIKEY = os.getenv("OPENAI_API_KEY")
 
-CRED = credentials.Certificate('./serviceAccountKey.json')
-firebase_admin.initialize_app(CRED, {
-    'storageBucket': 'lifehappens-293da.appspot.com'
-})
+# CRED = credentials.Certificate('./serviceAccountKey.json')
+# firebase_admin.initialize_app(CRED, {
+#     'storageBucket': 'lifehappens-293da.appspot.com'
+# })
 
 def get_file_content_from_fb(fb_path):
     try:
